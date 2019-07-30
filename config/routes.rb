270682489c5e5193acb3ce26ae30a48df3125d7a
minacon <minacon655@gmail.com>
2019-07-30 11:58:49 +0900

    update routes&productscontroller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'products#index'
-  resources :products, only: [:index, :new]
+  resources :products, only: [:index, :new] do
+    collection do
+      get 'buy'
+    end
+  end
   
 end
