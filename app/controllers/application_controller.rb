8ce5dd5723fb260e5278ead6dfce3dfc251df274
MSuzu190331 <m.suzuki.0u0@gmail.com>
2019-07-30 17:14:01 +0900

    create views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -13,4 +15,14 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  protected
+
+    def configure_permitted_parameters
+      # sign_upのときに、nameをストロングパラメータに追加する
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :prefecture, :address, :birth_date])
+      # account_updateのときに、nameをストロングパラメータに追加する
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
+    end
+
 end
