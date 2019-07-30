eb067d9bdf5c05dca7ec5377fd0cef0d4fb15205
MSuzu190331 <m.suzuki.0u0@gmail.com>
2019-07-31 00:00:21 +0900

    update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user! 
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birth_date, :address, :prefecture])
@@ -24,9 +23,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      # sign_upのときに、nameをストロングパラメータに追加する
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :prefecture, :address, :birth_date])
-      # account_updateのときに、nameをストロングパラメータに追加する
       devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
     end
 
