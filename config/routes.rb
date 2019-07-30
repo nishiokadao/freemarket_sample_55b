322a953b329abc1c831a5071e633af0b24ccee71
minacon <minacon655@gmail.com>
2019-07-30 17:12:48 +0900

    update routes&userscontroller

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
+  resources :users, only: [:show] do
+    collection do
+      get 'profile'
+    end
+  end
+  
 end
