f452d7216199bd0c2e28ab743984487aba8bd555
naomi-0913 <upworld409019@gmail.com>
2019-07-31 21:50:12 +0900

    create.first.html.haml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
   def show
+  end
 
+  def first
+  end
+
+  def second
+  end
+
+  def third
+  end
+
+  def forth
+  end
+
+  def fifh
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,19 @@ Rails.application.routes.draw do
   devise_for :users,
   controllers: {
     sessions: 'users/sessions',
-    omniauth_callbacks: 'users/omniauth_callbacks',
-    registrations: 'users/registrations'
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
-  resources :users, only: [:show]
+  resources :users, only: [:show, :new] do
+    collection do
+      get 'first'
+      get 'second'
+      get 'third'
+      get 'forth'
+      get 'fifith'
+    end
+  end
+
 end
