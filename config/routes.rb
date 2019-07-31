b039b4d9a5a4fedbeebb21d4b1fb3ca0b88433ae
morimorimokenpi <aaa.ys40.y2@gmail.com>
2019-07-31 19:03:20 +0900

    [change]detailのルーティングを追加しました。

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    collection do
+      get 'detail'
+    end
+  end
 end
