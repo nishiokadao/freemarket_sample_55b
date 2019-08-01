4d720422b1d171042a11f32308a0b12574fb70dd
minacon <minacon655@gmail.com>
2019-08-01 19:21:19 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,20 +3,18 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  # if Rails.env.development?
-  #   config.storage= :file
-  # elsif Rails.env.test?
-  #   config.storage= :file
-  # else
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: 'Rails.application.credentials.aws_access_key_id',
-    aws_secret_access_key: 'Rails.application.credentials.aws_secret_access_key',
-    region: 'ap-northeast-1'
-  }
-  # end
+  if Rails.env.development? || Rails.env.test?
+    config.storage= :file
+  else
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws_access_key_id,
+      aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
+      region: 'ap-northeast-1'
+    }
+  end
 
   config.fog_directory  = 'onigiri55b'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/onigiri55b'
