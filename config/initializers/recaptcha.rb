da1b699d873bf33b87b0cf8931faaf2509dfcfa7
naomi-0913 <upworld409019@gmail.com>
2019-08-02 12:31:16 +0900

    up

--- config/initializers/recaptcha.rb
+++
@@ -1,6 +0,0 @@
-Recaptcha.configure do |config|
-  config.credentials = {
-    site_key: Rails.application.credentials.RECAPTCHA_PUBLIC_KEY,
-    secret_key:Rails.application.credentials.RECAPTCHA_PRIVATE_KEY
-  }
-end
