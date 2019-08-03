593866cb8fe8748847777a08e2b095691f4b570f
naomi-0913 <upworld409019@gmail.com>
2019-08-03 16:47:41 +0900

    update-models

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
+  has_many :products
+
   # def self.from_omniauth(auth)
   #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
   #     user.uid = auth.uid
@@ -21,7 +23,7 @@ class User < ApplicationRecord
   #     user.birth_date = "1111"
   #   end
   # end
-
+  
   def self.from_omniauth(auth)
     User.where(provider: auth.provider, uid: auth.uid).first
   end
@@ -39,4 +41,5 @@ class User < ApplicationRecord
       end
     end
   end
+
 end
