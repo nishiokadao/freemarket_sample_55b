00156d4e35f60aaadfa0bb4f0f10929c8e254eb0
naomi-0913 <upworld409019@gmail.com>
2019-08-03 16:47:41 +0900

    update-models

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,10 +8,29 @@ class ProductsController < ApplicationController
   end
   
   def new
-    
+    @product = Product.new
+    @product.images.build
+    @product.build_delivery
+  end
+
+  def create
+    @product= Product.new(product_params)
+    if @product.save!
+      delivery_id = Delivery.find(@product.id).id                       #作成したdeliveryのIDを取り出す
+      product = Product.find(@product.id)                               #作成したItemのidを取り出す
+      product.update(delivery_id: delivery_id)    #productテーブルにderivery_idのカラムを入れる
+      redirect_to root_path
+    else
+      redirect_to new_product_path
+    end
   end
 
   def buy
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :condition, :price, image_attributes: [:id, :image], delivery_attributes: [:days_to_ship, :mode, :delivery_method,:shipping_place]).merge(seller_id: current_user.id)
+  end
   
   end
-end
