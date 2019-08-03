7b14656d40e3f829a8fdc856b4e9344064325179
minacon <minacon655@gmail.com>
2019-08-03 13:48:46 +0900

    update migration(products修正)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_040202) do
+ActiveRecord::Schema.define(version: 2019_08_03_044352) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -61,17 +61,19 @@ ActiveRecord::Schema.define(version: 2019_08_03_040202) do
     t.integer "postage", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "image_id"
-    t.bigint "delivery_id"
     t.integer "buyer_id"
     t.integer "seller_id", null: false
-    t.index ["delivery_id"], name: "index_products_on_delivery_id"
-    t.index ["image_id"], name: "index_products_on_image_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "avator"
+    t.string "prefecture", null: false
+    t.string "address", null: false
+    t.string "birth_date", null: false
+    t.text "introduction"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -85,5 +87,4 @@ ActiveRecord::Schema.define(version: 2019_08_03_040202) do
 
   add_foreign_key "category_products", "categories"
   add_foreign_key "category_products", "products"
-  add_foreign_key "products", "images"
 end
