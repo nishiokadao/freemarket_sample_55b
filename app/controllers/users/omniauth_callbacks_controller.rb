220edd60b517c7c45ff85efd3ccd29c3808bd210
morimorimokenpi <aaa.ys40.y2@gmail.com>
2019-08-03 22:05:13 +0900

    [change]omniauth認証の記述を変更
    ["extra"]と["raw_info"]の情報が取れなかったため記述を変更しました。

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,16 +14,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   #共通の設定
   def callback_from(provider)
     # provider = provider.to_s
-    # @user = User.from_omniauth(request.env['omniauth.auth'])
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user
       sign_in_and_redirect @user, event: :authentication
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
     else
-      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
+      session["devise.omniauth_data"] = request.env["omniauth.auth"].except("extra")
       @user = User.new()
-      redirect_to first_users_path
-      # render 'users/first'
+      redirect_to new_user_registration_path
     end
   end
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ApplicationRecord
   
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.omniauth_data"] && session["devise.omniauth_data"]["extra"]["raw_info"]
+      if data = session["devise.omniauth_data"] && session["devise.omniauth_data"]["info"]
         user.name = data["name"] if user.name.blank?
         user.email = data["email"] if user.email.blank?
       end
