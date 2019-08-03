7dc0d4e1b391757b3d7dd1aaaa43437b7a6297ec
naomi-0913 <upworld409019@gmail.com>
2019-08-03 20:35:34 +0900

    rebase.master

--- db/migrate/20190803100807_add_product_id_to_deliveries.rb
+++
@@ -1,5 +0,0 @@
-class AddProductIdToDeliveries < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :deliveries, :product, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -74,8 +74,6 @@ ActiveRecord::Schema.define(version: 2019_08_03_104900) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "avator"
-    t.text "introduction"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
