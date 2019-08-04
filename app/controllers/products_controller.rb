93b2eaf2cf45a642db6eb429ebd2efedb381f588
naomi-0913 <upworld409019@gmail.com>
2019-08-04 16:50:17 +0900

    set-create(カテゴリー保存はできない）

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,16 +9,17 @@ class ProductsController < ApplicationController
   
   def new
     @product = Product.new
-    @product.images.build
+    @product.build_image
+    @product.categories.build
     @product.build_delivery
   end
 
   def create
     @product= Product.new(product_params)
     if @product.save!
-      delivery_id = Delivery.find(@product.id).id                       #作成したdeliveryのIDを取り出す
-      product = Product.find(@product.id)                               #作成したItemのidを取り出す
-      product.update(delivery_id: delivery_id)    #productテーブルにderivery_idのカラムを入れる
+      # delivery_id = Delivery.find(@product.id).id       #作成したdeliveryのIDを取り出す
+      # product = Product.find(@product.id)               #作成したItemのidを取り出す
+      # product.update(delivery_id: delivery_id)          #productテーブルにderivery_idのカラムを入れる
       redirect_to root_path
     else
       redirect_to new_product_path
@@ -30,7 +31,8 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :description, :condition, :price, image_attributes: [:id, :image, :product_id], delivery_attributes: [:days_to_ship, :mode, :delivery_method,:shipping_place]).merge(seller_id: current_user.id)
+    params.require(:product).permit(:name, :description, :condition, :price, category_attributes: [:id, :name], image_attributes: [:id, :image, :product_id], delivery_attributes: [:id, :days_to_ship, :mode, :delivery_method,:shipping_place]).merge(seller_id: current_user.id)
   end
-  
-  end
+
+
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ApplicationRecord
-  belongs_to :user
-  has_many :images
-  accepts_nested_attributes_for :images
+  belongs_to :user, optional: true
+  has_one :image
+  accepts_nested_attributes_for :image
   has_one :delivery
   accepts_nested_attributes_for :delivery
   has_many :category_products
   has_many :categories, through: :category_products
+
 end
