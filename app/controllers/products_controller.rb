aa708cd52c6e6631b2225c1e9422f6ac7361a85b
minacon <minacon655@gmail.com>
2019-08-04 11:46:39 +0900

    payj実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,15 @@ class ProductsController < ApplicationController
   end
 
   def buy
-  
+    @product = Product.find(1)
+  end
+
+  def pay
+    Payjp.api_key = 'sk_test_634d5041b80a0c0fca6d2552'
+    Payjp::Charge.create(
+    amount:700,
+    card:params['payjp-token'],
+    currency:'jpy',
+    )
   end
 end
