e223e88f5dbe54c17808418b73b77b98d156f9fd
minacon <minacon655@gmail.com>
2019-08-05 16:22:41 +0900

    Update productscontroller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   end
 
   def show
-    
+    @product = product.find(params[id])
   end
   
   def new
@@ -16,11 +16,23 @@ class ProductsController < ApplicationController
   end
 
   def pay
-    Payjp.api_key = ENV['PAYJ_SECRET_KEY']
-    Payjp::Charge.create(
-    amount:700,
-    card:params['payjp-token'],
-    currency:'jpy',
-    )
+    begin
+      Payjp.api_key = ENV['PAYJ_SECRET_KEY']
+      Payjp::Charge.create(
+      amount:700,
+      card:params['payjp-token'],
+      currency:'jpy',
+      )
+    rescue => exception
+      redirect_to  buy_products_path
+    end
+    
   end
+
+  private 
+  def product_params
+    params.require(:product).permit(:name, :description, :condition, :price, category_attributes: [:id, :name], image_attributes: [:id, :image, :product_id], delivery_attributes: [:id, :days_to_ship, :mode, :delivery_method,:shipping_place]).merge(seller_id: current_user.id)
+  end
+
+
 end
