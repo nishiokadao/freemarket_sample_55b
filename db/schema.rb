091e385c2014df18311fc5078542fd3c483ff42e
minacon <minacon655@gmail.com>
2019-08-06 21:28:39 +0900

    creditテーブルカラム型変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_05_043810) do
+ActiveRecord::Schema.define(version: 2019_08_06_082552) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -31,8 +31,8 @@ ActiveRecord::Schema.define(version: 2019_08_05_043810) do
   create_table "credits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "payjp_id", null: false
-    t.integer "card_id", null: false
+    t.string "payjp_id", null: false
+    t.string "card_id", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_credits_on_user_id"
   end
