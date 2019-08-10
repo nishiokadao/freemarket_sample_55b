82ff51da5c194049b3d58cdd64b94f2285d72eb9
morimorimokenpi <aaa.ys40.y2@gmail.com>
2019-08-10 18:07:27 +0900

    [change]prefix名を設定

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   end
   resources :products, only: [:index, :new, :show, :create, :edit, :update, :destroy] do
     collection do
-      get 'exhibit/:id' => 'products#exhibit', as: 'imokenpi'
+      get 'exhibit/:id' => 'products#exhibit', as: 'exhibit'
       get 'buy/:id' => 'products#buy' 
       get 'pay/:id' => 'products#pay'
     end
