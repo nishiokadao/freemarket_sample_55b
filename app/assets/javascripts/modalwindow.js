a7b4f6627a3dd5c61091d9e25393e9803d88a523
morimorimokenpi <aaa.ys40.y2@gmail.com>
2019-08-11 22:02:30 +0900

    [change]turbolinksを追加

--- app/assets/javascripts/modalwindow.js
+++ app/assets/javascripts/modalwindow.js
@@ -1,13 +1,10 @@
-document.addEventListener(
-  'DOMContentLoaded', (e) => {
-    let modal_open = document.getElementById('modal_open')
-    let modal_close = document.getElementById('modal-close-btn')
-    modal_open.onclick = function () {
-      $('#overlay').fadeIn();
-    };
-    modal_close.onclick = function () {
-      $('#overlay').fadeOut();
-    };
-  },
-  false
-);
+$(document).on('turbolinks:load', function(){
+  let modal_open = document.getElementById('modal_open')
+  let modal_close = document.getElementById('modal-close-btn')
+  modal_open.onclick = function () {
+    $('#overlay').fadeIn();
+  };
+  modal_close.onclick = function () {
+    $('#overlay').fadeOut();
+  };
+});
