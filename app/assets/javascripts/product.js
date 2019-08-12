eacec0691b138ef992890a4d5b360adc3df41a57
naomi-0913 <upworld409019@gmail.com>
2019-08-12 23:59:17 +0900

    update.js

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -1,26 +1,159 @@
 $(document).on('turbolinks:load', function(){
 
+  // 投稿クリック範囲
   var postBtn = document.getElementById('click-btn');
-
   postBtn.addEventListener('click', function(){
-    $('#post__image').click();
+    $('#post-image').click();
   });
 
-  function readURL(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-      reader.onload = function (e) {
-        $('.upload-image__prev--figure--src').attr('src', e.target.result);
-      }
-      reader.readAsDataURL(input.files[0]);
+  // 複数表示
+
+  var dropzone = $('.dropzone-area');
+  var dropzone2 = $('.dropzone-area2');
+  var images = [];
+  var inputs  =[];
+  var input_area = $('.input_area');
+  var preview = $('#preview');
+  var preview2 = $('#preview2');
+
+  $(document).on('change', 'input[type= "file"].upload-image',function(event) {
+    var file = $(this).prop('files')[0];
+    var reader = new FileReader();
+    inputs.push($(this));
+    var img = $(`<div class= "img_view"><img></div>`);
+    reader.onload = function(e) {
+      var btn_wrapper = $('<div class="btn_wrapper"><div class="btn edit">編集</div><div class="btn delete">削除</div></div>');
+      img.append(btn_wrapper);
+      img.find('img').attr({
+        src: e.target.result
+      })
     }
-  }
+    reader.readAsDataURL(file);
+    images.push(img);
 
-  $('#post__image').change(function(){
-    $('.sellbox-form__upload--drop--box--image').removeAttr('style');
-    readURL(this);
+    if(images.length >= 5) {
+      dropzone2.css({
+        'display': 'block'
+      })
+      dropzone.css({
+        'display': 'none'
+      })
+      $.each(images, function(index, image) {
+        image.attr('data-image', index);
+        preview2.append(image);
+        dropzone2.css({
+          'width': `calc(100% - (135px * ${images.length - 5}))`
+        })
+      })
+      if(images.length == 9) {
+        dropzone2.find('p').replaceWith('<i class="fa fa-camera"></i>')
+      }
+    } else {
+        $('#preview').empty();
+        $.each(images, function(index, image) {
+          image.attr('data-image', index);
+          preview.append(image);
+        })
+        dropzone.css({
+          'width': `calc(100% - (135px * ${images.length}))`
+        })
+      }
+      if(images.length == 4) {
+        dropzone.find('p').replaceWith('<i class="fa fa-camera"></i>')
+      }
+    if(images.length == 10) {
+      dropzone2.css({
+        'display': 'none'
+      })
+      return;
+    }
+    var new_image = $(`<input multiple= "multiple" name="product_images[image][]" class="upload-image" data-image= ${images.length} type="file" id="upload-image">`);
+    input_area.prepend(new_image);
   });
+  // 削除
+  $(document).on('click', '.delete', function() {
+    var target_image = $(this).parent().parent();
+    $.each(inputs, function(index, input){
+      if ($(this).data('image') == target_image.data('image')){
+        $(this).remove();
+        target_image.remove();
+        var num = $(this).data('image');
+        images.splice(num, 1);
+        inputs.splice(num, 1);
+        if(inputs.length == 0) {
+          $('input[type= "file"].upload-image').attr({
+            'data-image': 0
+          })
+        }
+      }
+    })
+    $('input[type= "file"].upload-image:first').attr({
+      'data-image': inputs.length
+    })
+    $.each(inputs, function(index, input) {
+      var input = $(this)
+      input.attr({
+        'data-image': index
+      })
+      $('input[type= "file"].upload-image:first').after(input)
+    })
+    if (images.length >= 5) {
+      dropzone2.css({
+        'display': 'block'
+      })
+      $.each(images, function(index, image) {
+        image.attr('data-image', index);
+        preview2.append(image);
+      })
+      dropzone2.css({
+        'width': `calc(100% - (135px * ${images.length - 5}))`
+      })
+      if(images.length == 9) {
+        dropzone2.find('p').replaceWith('<i class="fa fa-camera"></i>')
+      }
+      if(images.length == 8) {
+        dropzone2.find('i').replaceWith('<p>ココをクリックしてください</p>')
+      }
+    } else {
+      dropzone.css({
+        'display': 'block'
+      })
+      $.each(images, function(index, image) {
+        image.attr('data-image', index);
+        preview.append(image);
+      })
+      dropzone.css({
+        'width': `calc(100% - (135px * ${images.length}))`
+      })
+    }
+    if(images.length == 4) {
+      dropzone2.css({
+        'display': 'none'
+      })
+    }
+    if(images.length == 3) {
+      dropzone.find('i').replaceWith('<p>ココをクリックしてください</p>')
+    }
+  })
 
+  // function readURL(input) {
+  //   if (input.files && input.files[0]) {
+  //     var reader = new FileReader();
+  //     reader.onload = function (e) {
+  //       $('.upload-image__prev--figure--src').attr('src', e.target.result);
+  //     }
+  //     reader.readAsDataURL(input.files[0]);
+  //   }
+  // }
+
+  // $('#post__image').change(function(){
+  //   $('.sellbox-form__upload--drop--box--image').show();
+  //   $('.sellbox-form__upload--drop--post').width('66%');
+  //   readURL(this);
+  // });
+
+
+  // 手数料・利益表示
   $('#product_price').on('keyup', function(){
     var input= $(this).val();
     var commission = "¥" + Math.round(input*0.1);
@@ -33,7 +166,7 @@ $(document).on('turbolinks:load', function(){
     $('.price-group__box--right').empty();
     $('.price-group__box--bold--right').empty();
   });
-
+    // モーダルウィンドウ
   $('#new_product').on('submit', function(e){
     e.preventDefault();
     var product = new FormData(this);
