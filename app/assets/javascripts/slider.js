08b5cb6973efe94dedecef8526e7623f68cebe94
morimorimokenpi <aaa.ys40.y2@gmail.com>
2019-08-14 19:01:36 +0900

    [change]スライダーさせるクラス変更

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,6 +1,6 @@
 $(function() {
   $(document).on('turbolinks:load', function(){
-    $('.slider-contents').slick({
+    $('.index-slider-contents').slick({
       prevArrow: '<button class ="prev-arrow"><img src="https://www.mercari.com/jp/assets/img/common/jp/top/carousel-prev.png"></button>',
       nextArrow: '<button class ="next-arrow"><img src="https://www.mercari.com/jp/assets/img/common/jp/top/carousel-next.png"></button>',
       autoplay: true,
