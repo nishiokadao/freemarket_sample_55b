04353da4c765ab46549ed12dc7c14f5f4b7bb7f2
morimorimokenpi <aaa.ys40.y2@gmail.com>
2019-08-14 17:11:04 +0900

    [remove]不要なドット削除

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -3,8 +3,8 @@ $(function() {
     $('.slider-contents').slick({
       prevArrow: '<button class ="prev-arrow"><img src="https://www.mercari.com/jp/assets/img/common/jp/top/carousel-prev.png"></button>',
       nextArrow: '<button class ="next-arrow"><img src="https://www.mercari.com/jp/assets/img/common/jp/top/carousel-next.png"></button>',
-      dots: true,
-      dotsClass: 'slide-dots'
+      autoplay: true,
+      autoplaySpeed: 10000,
     });
   });
 });
